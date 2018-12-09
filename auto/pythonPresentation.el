f743ad62c3053320e59bf466a45dca2f301a8ec7
Bibek Gautam <forgot.bg@gmail.com>
2018-12-09 22:38:03 +0545

    images

---
+++ auto/pythonPresentation.el
@@ -0,0 +1,34 @@
+(TeX-add-style-hook
+ "pythonPresentation"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "12pt" "aspectratio=169")))
+   (add-to-list 'LaTeX-verbatim-environments-local "minted")
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer12"
+    "listings"
+    "minted"
+    "tikz")
+   (LaTeX-add-labels
+    "fig:py"
+    "fig:c")
+   (LaTeX-add-xcolor-definecolors
+    "secinhead"
+    "foot"
+    "footfg"
+    "titlebg"))
+ :latex)
+
